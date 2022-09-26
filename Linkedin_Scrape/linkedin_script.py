d4202d649c378c408571adcf914c3dda8560c3ed
Amin <amin.hakim.yahya@gmail.com>
2022-09-26 22:15:09 +0800

    latest version with next click button

--- Linkedin_Scrape/linkedin_script.py
+++ Linkedin_Scrape/linkedin_script.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 import pandas as pd
 
@@ -7,12 +9,16 @@ path = '\Python310\Chromedriver'
 driver = webdriver.Chrome(executable_path=r'C:\Python310\Chromedriver\chromedriver.exe')
 driver.get(website)
 
+time.sleep(2)
+
 # click sign in button
 click_sign_in = driver.find_element("xpath", '//*[@class="main__sign-in-link"]').click()
 
 # fill linkedin username and password
-username = "tsubaki.azura@gmail.com"
-password = "F@w6UZ*F4kfY"
+username = "pringiolos@gmail.com"
+password = "Akk1Q@gtBHPV"
+
+time.sleep(5)
 
 # automated fill in username and password
 driver.find_element("xpath", '//*[@name="session_key"]').send_keys(username)
@@ -61,9 +67,55 @@ print('DESCRIPTION')
 for a in description:
     print(a)
 
-df = pd.DataFrame({'company_name': company_name, 'follower': follower, 'industry': industry, 'description': description,})
-df.to_excel('test1.xlsx', index=False)
-print(df)
+# scroll to the bottom of the page
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
+# wait before click next
+time.sleep(5)
+
+# click next button
+next_button = driver.find_element("xpath", '//button[@aria-label="Next"]').click()
+
+time.sleep(2)
+
+scrape = driver.find_elements("xpath", '//div[@class="entity-result__content entity-result__divider pt3 pb3 t-12 t-black--light"]')
+
+company_name = []
+follower = []
+industry = []
+description = []
+
+for a in scrape:
+    array1 = a.text.split('\n')
+    #print(array1)
+    company_name.append(array1[0])
+    follower.append(array1[1])
+    industry.append(array1[2])
+    description.append(array1[3])
+    #print(a.text)
+
+#print(company_name)
+#print(follower)
+#print(industry)
+#print(description)
+print('COMPANY NAME')
+for a in company_name:
+    print(a)
+print()
+print('FOLLOWER')
+for a in follower:
+    print(a)
+print()
+print('INDUSTRY')
+for a in industry:
+    print(a)
+print()
+print('DESCRIPTION')
+for a in description:
+    print(a)
+
+df = pd.DataFrame({'company_name': company_name, 'follower': follower, 'industry': industry, 'description': description,})
+df.to_excel('test3.xlsx')
+print(df)
 
 
