5e860ee24cc2e96e8b19df0ced630f9d28ccbc34
Amin <amin.hakim.yahya@gmail.com>
2022-09-26 21:00:43 +0800

    first page scrape

--- Linkedin_Scrape/linkedin_script.py
+++ Linkedin_Scrape/linkedin_script.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from time import sleep
+import pandas as pd
 
 # open target website with webdriver
 website = 'https://www.linkedin.com/search/results/companies/?heroEntityKey=urn%3Ali%3Aautocomplete%3A-1781024605&keywords=video%20production&origin=CLUSTER_EXPANSION&position=0&searchId=8b8b2aaa-172b-4aae-86ba-842fcb3ce178&sid=gac'
@@ -14,8 +11,8 @@ driver.get(website)
 click_sign_in = driver.find_element("xpath", '//*[@class="main__sign-in-link"]').click()
 
 # fill linkedin username and password
-username = "pringiolos@gmail.com"
-password = "Akk1Q@gtBHPV"
+username = "tsubaki.azura@gmail.com"
+password = "F@w6UZ*F4kfY"
 
 # automated fill in username and password
 driver.find_element("xpath", '//*[@name="session_key"]').send_keys(username)
@@ -31,10 +28,42 @@ click_submit = driver.find_element("xpath", '//*[@aria-label="Sign in"]').click(
 scrape = driver.find_elements("xpath", '//div[@class="entity-result__content entity-result__divider pt3 pb3 t-12 t-black--light"]')
 
 company_name = []
+follower = []
+industry = []
+description = []
 
 for a in scrape:
-    print(a.text)
+    array1 = a.text.split('\n')
+    #print(array1)
+    company_name.append(array1[0])
+    follower.append(array1[1])
+    industry.append(array1[2])
+    description.append(array1[3])
+    #print(a.text)
 
+#print(company_name)
+#print(follower)
+#print(industry)
+#print(description)
+print('COMPANY NAME')
+for a in company_name:
+    print(a)
+print()
+print('FOLLOWER')
+for a in follower:
+    print(a)
+print()
+print('INDUSTRY')
+for a in industry:
+    print(a)
+print()
+print('DESCRIPTION')
+for a in description:
+    print(a)
+
+df = pd.DataFrame({'company_name': company_name, 'follower': follower, 'industry': industry, 'description': description,})
+df.to_excel('test1.xlsx', index=False)
+print(df)
 
 
 
